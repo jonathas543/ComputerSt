e6091a3d57f5c4654c73f6c60724e8a52f4e3592
jonathas543 <jonathasdasilvanunes@gmail.com>
2026-04-18 22:26:36 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ let buttonPrev = document.getElementById('1seta')
 let Nextbutton = document.getElementById('2seta')
 let tudo3 = document.querySelector('.tudo3')
 let itens = tudo3.querySelectorAll('.lista .Itens')
-let indicators = document.querySelector('.indicators')
-let indicators1 = indicators.querySelectorAll('ul li')
+let indicator = document.querySelector('.indicators')
+let indicators1 = indicator.querySelectorAll('ul li')
 
 
 let active = 0
@@ -18,7 +18,41 @@ Nextbutton.onclick = () => {
     active = active + 1 > last ? 0 : active + 1
     
     itens[active].classList.add('active')
-}
-buttonPrev.onclick = () => {
+
+
+    let indic = indicator.querySelector('ul li.active')
+
+    indic.classList.remove('active')
     
+    indicators1[active].classList.add('active')
+
+    indicator.querySelector('.number').innerHTML = '0'+ (active + 1)
+
 }
+
+buttonPrev.onclick = () => {
+    let itensOld = tudo3.querySelector('.lista .Itens.active')
+    itensOld.classList.remove('active')
+
+    active = active - 1 < 0 ? 7 : active - 1
+
+    itens[active].classList.add('active')
+
+
+
+
+
+    let indic = indicator.querySelector('ul li.active')
+
+    indic.classList.remove('active')
+    
+    indicators1[active].classList.add('active')
+
+    indicator.querySelector('.number').innerHTML = '0'+ (active + 1)
+
+}
+
+
+
+
+
